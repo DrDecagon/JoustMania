f0687f85562621964a0f88f5646cb1a7cd332cb6
DrDecagon <DrDecagon@users.noreply.github.com>
2023-06-20 04:38:12 -0300

    boost cpu performance

--- joust.sh
+++ joust.sh
@@ -5,6 +5,11 @@ if [ $UID -ne 0 ]; then
   exec sudo $0
 fi
 
+echo "Setting CPU performance mode on all cores"
+echo "performance" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo "Setting max temp"
+echo 70000 | tee /sys/class/thermal/thermal_zone0/trip_point_0_temp
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #supervisord does not have a login name
 #so we need to find the JoustMania install directory
