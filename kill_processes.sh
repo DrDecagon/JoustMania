2ca3f5ceeee7b6d2c9809c02e960814687648304
DrDecagon <DrDecagon@users.noreply.github.com>
2023-06-23 04:33:02 -0300

    testing new kill method

--- kill_processes.sh
+++ kill_processes.sh
@@ -3,4 +3,8 @@
 #this is for development purposes only, to stop the automattically
 #running piparty scripts
 sudo supervisorctl stop joustmania
-sudo kill $(ps aux | grep 'piparty' | awk '{print $2}')
+#sudo kill -TERM $(ps aux | grep 'piparty' | awk '{print $2}')
+#sleep 2
+sudo kill -QUIT $(ps aux | grep 'piparty' | awk '{print $2}')
+#sleep 4
+#sudo kill -KILL $(ps aux | grep 'piparty' | awk '{print $2}')
